b2bcc2bab7ddde17ba28058d1c002fcf562f2ece
Kambocha <kambocha7@gmail.com>
2015-01-08 16:44:11 -0800

    v1.0

---
+++ FirstCalculator/First Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+            Console.WriteLine("Enter the first number: ");
+            num = Convert.ToDouble(Console.ReadLine());
+
+            int symbol;
+            Console.WriteLine("Enter the Symbol: ");
+            symbol = Convert.ToChar(Console.ReadLine());
+
+            double numtwo;
+            Console.WriteLine("Enter the second number: ");
+            numtwo = Convert.ToDouble(Console.ReadLine());
+
+            if (symbol == '+')
+            {
+                Console.WriteLine(num + numtwo);
+            }
+            else if (symbol == '-')
+            {
+                Console.WriteLine(num - numtwo);
+            }
+            else if (symbol == '/')
+            {
+                Console.WriteLine(num / numtwo);
+            }
+            else if (symbol == '*')
+            {
+                Console.WriteLine(num * numtwo);
+            }
+        }
+    }
+}
